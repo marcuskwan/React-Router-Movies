42be58c1f3dde49bb4d9837b6f86165ec63595aa
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-12 09:04:47 +1000

    added functionality to addsavedlist func

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,12 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:banana" component={Movie} />
+        <Route
+          path="/movies/:banana"
+          render={props => (
+            <Movie {...props} addToSavedList={this.addToSavedList} />
+          )}
+        />
       </div>
     );
   }
